4cb3676190d07f45f5f84881382d58773ff63051
Temuulen8 <b.temuulen2005@gmail.com>
2024-09-06 12:59:59 +0800

    cards

--- expense-front/src/app/about/components/header.jsx
+++ expense-front/src/app/about/components/header.jsx
@@ -8,26 +8,25 @@ export const Header = ({ user, logOut }) => {
       <div className="flex gap-6 items-center">
         <Image src="/Vector.png" width={28} height={28} alt="logo" />
         <Link href="/dashboard">
-          <p>Dashboard</p>
+          <p className="font-semibold">Dashboard</p>
         </Link>
         <Link href="/records">
           <p>Records</p>
         </Link>
       </div>
       <div className="flex gap-6 items-center">
-        <button className="btn bg-[#0166FF] text-white btn-sm">
+        <button className="btn bg-[#0166FF] text-white btn-sm flex items-center rounded-[20px] px-3 py-1">
           <PlusIcon />
           Records
         </button>
-        <div className="avatar w-12 h-12">
-          <div className="w-24 rounded-full">{user.name}</div>
-          <div className="w-24 rounded-full">
-            <img src={user?.avatarImg} />
+        <div className="flex gap-9">
+          <div className="avatar w-12 h-12 flex items-center gap-2 font-medium">
+            {user.name}
           </div>
+          <button className="btn btn-sm" onClick={logOut}>
+            Log out
+          </button>
         </div>
-        <button className="btn btn-sm" onClick={logOut}>
-          Log out
-        </button>
       </div>
     </header>
   );
--- expense-front/src/app/dashboard/page.jsx
+++ expense-front/src/app/dashboard/page.jsx
@@ -30,9 +30,73 @@ const Dashboard = () => {
   }, [user.id]);
 
   return (
-    <div>
+    <div className="">
       <div>
-        <h2>Records</h2>
+        <div className="flex justify-center">
+          <div className="flex gap-6">
+            <div className="w-[384px] h-[216px] bg-[#0166FF] rounded-[18px]">
+              <div className="py-6 pl-4 flex flex-col gap-20">
+                <img
+                  className="w-[80.35px] h-[29.86px]"
+                  src="/cardlogo.png"
+                  alt=""
+                />
+                <div>
+                  <p className="text-[#FFFFFF]">cash</p>
+                  <div className="flex gap-60">
+                    <p className="text-[#FFFFFF]">10,000,00</p>
+                    <img src="/Union.png" alt="" />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="w-[384px] h-[216px] rounded-[18px] border">
+              <div className="h-[56px] border-b-[1px] flex items-center">
+                <p className="pl-5 flex items-center gap-2">
+                  <img src="/dotg.png" alt="" />
+                  <p>Your income</p>
+                </p>
+              </div>
+              <div className="flex flex-col pl-4 gap-4 pt-3">
+                <div className="font-semibold text-4xl">1,200,000</div>
+                <div className="text-[#64748B]">Your Income Amount</div>
+                <div className="flex items-center">
+                  <img src="/up.png" alt="" />
+                  <div>32% from last month</div>
+                </div>
+              </div>
+            </div>
+            <div className="w-[384px] h-[216px] rounded-[18px] border">
+              <div className="h-[56px] border-b-[1px] flex items-center">
+                <p className="pl-5 flex items-center gap-2">
+                  <img src="/dotb.png" alt="" />
+                  <p>Total expenses</p>
+                </p>
+              </div>
+              <div className="flex flex-col pl-4 gap-4 pt-3">
+                <div className="font-semibold text-4xl">-1,200,00</div>
+                <div className="text-[#64748B]">Your expence Amount</div>
+                <div className="flex items-center">
+                  <img src="/down.png" alt="" />
+                  <div>32% from last month</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="flex justify-center pt-6">
+          <div className="w-[1200px] rounded-xl border">
+            <div className="border-b-[1px]">Last Records</div>
+            <div className="flex">
+              <img src="/home.png" alt="" />
+              <div>
+                <p>Lending & Renting</p>
+                <p>3 hours ago</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
         {transactionData?.transactions?.map((transaction, index) => {
           return (
             <div key={index} className="flex">
